6c1879a7a3de74b8da3552350508d4f4e96360a0
Yash Dayani <52958495+YashDayani@users.noreply.github.com>
2024-07-03 16:06:57 +0530

    03-July-24

--- 3. EcommerceStore/src/components/hero/Hero.jsx
+++ 3. EcommerceStore/src/components/hero/Hero.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './hero.css'
+import laysImg from '../../assets/images/lays.jpg'
 
 const Hero = () => {
     return (
         <section className="hero container">
             <div className="hero-main">
-                <div className="div1 hero-box">
-                    <span>NEW PRODUCT</span>
-                    <h1>Top </h1>
+                <div className="div1 hero-box hero-box-main">
+                    <div className='hero-box-product'>
+                        <span className='hero-product-status'>NEW PRODUCT</span>
+                        <h1 className='hero-product-title'>Top quality seafood <br /> from Royal Blue</h1>
+                        <div className='hero-product-info'>
+                            <span className='hero-product-category item-category'>Seafood & Fish</span><br />
+                            <span className='hero-product-name item-name'>Royal Blue Medium Shrimp</span><br />
+                            <span className='hero-product-price item-price'>$8.99</span>
+                        </div>
+                        <img className='hero-product-img' src={laysImg} alt="" />
+                        <a href="#" className='hero-details-btn round-btn'>DETAILS</a>
+                    </div>
+                    <div className='hero-box-img'></div>
                 </div>
                 <div className="div2 hero-box"></div>
                 <div className="div3 hero-box"></div>
--- 3. EcommerceStore/src/components/slider/Slider.jsx
+++ 3. EcommerceStore/src/components/slider/Slider.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import './slider.css'
 import arrowLeftIcon from '../../assets/images/arrow-left.svg'
 import arrowRightIcon from '../../assets/images/arrow-right.svg'
+import cat1Icon from '../../assets/images/cat1.png'
+import cat2Icon from '../../assets/images/cat2.png'
+import cat3Icon from '../../assets/images/cat3.png'
+import cat4Icon from '../../assets/images/cat4.png'
+import cat5Icon from '../../assets/images/cat5.png'
+import cat6Icon from '../../assets/images/cat6.png'
 
 const Slider = () => {
   return (
@@ -10,12 +16,30 @@ const Slider = () => {
             <img src={arrowLeftIcon} className='slider-button'/>
         </a>
         <div className="slider-items">
-            <div className="slider-item"></div>
-            <div className="slider-item"></div>
-            <div className="slider-item"></div>
-            <div className="slider-item"></div>
-            <div className="slider-item"></div>
-            <div className="slider-item"></div>
+            <div className="slider-item">
+              <img className='slider-item-img' src={cat1Icon} />
+              <span className='slider-item-name'>Fruits <br /> & Veges</span>
+            </div>
+            <div className="slider-item">
+              <img className='slider-item-img' src={cat2Icon} />
+              <span className='slider-item-name'>Herbs</span>
+            </div>
+            <div className="slider-item">
+              <img className='slider-item-img' src={cat3Icon} />
+              <span className='slider-item-name'>Dairy <br />Item</span>
+            </div>
+            <div className="slider-item">
+              <img className='slider-item-img' src={cat4Icon} />
+              <span className='slider-item-name'>Oils <br /> & Ghee</span>
+            </div>
+            <div className="slider-item">
+              <img className='slider-item-img' src={cat5Icon} />
+              <span className='slider-item-name'>Frozen <br /> Item</span>
+            </div>
+            <div className="slider-item">
+              <img className='slider-item-img' src={cat6Icon} />
+              <span className='slider-item-name'>Seeds <br /> & Nuts</span>
+            </div>
         </div>
         <a className='btn-round' href="#">
             <img src={arrowRightIcon} className='slider-button' />

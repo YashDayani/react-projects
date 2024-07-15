e57e92bbb2b5e22ea2ddcd6c7ec83d73e1e17b66
Yash Dayani <52958495+YashDayani@users.noreply.github.com>
2024-07-15 16:50:09 +0530

    14-July-24

---
+++ 3. EcommerceStore/src/pages/Faq.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './faq.css'
+
+
+const Faq = () => {
+  return (
+    <div className="container">
+      <div className="page-title-container">
+      <div className='page-title'>
+        FAQ
+      </div>
+      <div className="page-subtitle">
+        Your Questions Answered, Find Solutions and Information Below
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Faq
